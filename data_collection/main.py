11193fbd7ebf921fb9c3cb2df69fd7f2744d75ee
Nic Orchard <cash@GG-McAir.local>
2022-05-24 01:11:48 +0200

    files

--- data_collection/main.py
+++ data_collection/main.py
@@ -2,15 +2,16 @@ import twint
 import nest_asyncio
 import pandas as pd
 
-nest_asyncio.apply()
+# utiliza la libreria de TWINT para escrappear twitter y obtener la informacion deseada
 
-# Configure
+nest_asyncio.apply()
+# Configuracion
 c = twint.Config()
 c.Search = '@TheBridge_Tech'
-# c.Store_json = True
-# c.Output = '.data/raw_tweets.json'
+# Guardar en CSV
 c.Store_csv = True
-c.Output = './data/raw_tweets.csv'
+c.Output = '../data/raw_tweets.csv'
+# especificar fecha
 c.Since = '2022-01-01 00:00:00'
 c.Hide_output = True
 c.Debug = True
@@ -20,5 +21,3 @@ try:
     twint.run.Search(c)
 except Exception as e:
     print(e)
-
-data = pd.read_csv('../data/raw_tweets.csv')
