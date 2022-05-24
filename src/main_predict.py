45bdbf12b0eac4aeec19511b7fdb15644e4953f7
Nic Orchard <cash@GG-McAir.local>
2022-05-24 02:21:26 +0200

    presentacion

--- src/main_predict.py
+++ src/main_predict.py
@@ -2,7 +2,7 @@ import pickle
 from functions import read_data, top_tweets
 
 model = pickle.load(open('./model/finished_model.model', 'rb'))
-
+df = read_data('./data/clean_tweets.csv')
 x_test = top_tweets(df,3) 
 
 pred = model.predict(x_test)
